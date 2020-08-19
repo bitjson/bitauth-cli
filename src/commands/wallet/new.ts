68e8c22b729f1d58daaeac4b23af915b67b5ebb2
Jason Dreyzehner <jason@dreyzehner.com>
2020-08-18 21:47:06 -0400

    WIP

--- src/commands/wallet/new.ts
+++ src/commands/wallet/new.ts
@@ -1,6 +1,20 @@
+import { randomBytes } from 'crypto';
+
 import {
   AuthenticationTemplate,
   AuthenticationTemplateEntity,
+  binToHex,
+  CompilerDefaults,
+  deriveHdPath,
+  deriveHdPublicNode,
+  encodeHdPrivateKey,
+  encodeHdPublicKey,
+  generateHdPrivateNode,
+  generatePrivateKey,
+  instantiateRipemd160,
+  instantiateSecp256k1,
+  instantiateSha256,
+  instantiateSha512,
   validateAuthenticationTemplate,
 } from '@bitauth/libauth';
 import { Command, flags } from '@oclif/command';
@@ -10,10 +24,92 @@ import { parseJsonFlagOrFail } from '../../internal/flags';
 import {
   bashEscapeSingleQuote,
   colors,
+  formatJsonForSigning,
   toKebabCase,
 } from '../../internal/formatting';
 import { logger } from '../../internal/initialize';
 import { getTemplates } from '../../internal/storage';
+
+const ripemd160Promise = instantiateRipemd160();
+const secp256k1Promise = instantiateSecp256k1();
+const sha256Promise = instantiateSha256();
+const sha512Promise = instantiateSha512();
+
+const keyLength = 32;
+const random32Bytes = () => Uint8Array.from(randomBytes(keyLength));
+
+/**
+ * Ensure that `dataItem` is an object which contains all `expectedKeys`, has no
+ * unexpected keys, and all values are strings.
+ */
+const verifyDataItem = (
+  dataItem: unknown,
+  expectedKeys: string[]
+): string[] | { [key: string]: string } => {
+  if (typeof dataItem !== 'object' || dataItem === null) {
+    return ['Must be an object.'];
+  }
+
+  const includedKeys = Object.keys(dataItem);
+  const missingKeys = expectedKeys.map((expected) =>
+    includedKeys.includes(expected)
+      ? null
+      : `Missing required key: '${expected}'.`
+  );
+
+  const invalidKeys = Object.entries(dataItem).map(
+    (entry: [string, unknown]) => {
+      const [key, value] = entry;
+      if (typeof value !== 'string') {
+        return `Invalid key: '${key}' must be a BTL-encoded string.`;
+      }
+      if (!expectedKeys.includes(key)) {
+        return `Unexpected key: ${key}.`;
+      }
+      return null;
+    }
+  );
+
+  const errors = [...missingKeys, ...invalidKeys].filter(
+    (result): result is string => result !== null
+  );
+
+  return errors.length > 0 ? errors : (dataItem as { [key: string]: string });
+};
+
+/**
+ * Ensure that `addressData` is an array and that every element satisfies
+ * `verifyDataItem`.
+ */
+const verifyAddressData = (
+  addressData: unknown,
+  expectedKeys: string[]
+):
+  | { success: false; errors: string[] }
+  | { success: true; addressData: { [key: string]: string }[] } => {
+  if (!Array.isArray(addressData)) {
+    return {
+      errors: ['Must be an array of address data objects.'],
+      success: false,
+    };
+  }
+  const errors = addressData
+    .map((item, i) => {
+      const result = verifyDataItem(item, expectedKeys);
+      if (Array.isArray(result)) {
+        return `Invalid address data at index ${i}: ${result.join(' ')}`;
+      }
+      return null;
+    })
+    .filter((result): result is string => result !== null);
+
+  return errors.length > 0
+    ? { errors, success: false }
+    : {
+        addressData: addressData as { [key: string]: string }[],
+        success: true,
+      };
+};
 
 export default class WalletNew extends Command {
   static description = `create a new wallet`;
@@ -163,48 +259,48 @@ export default class WalletNew extends Command {
       );
     }
 
-    const partitionedVariables =
-      entity.variables === undefined
-        ? undefined
-        : Object.entries(entity.variables).reduce<{
-            addressData: { id: string }[];
-            walletData: { id: string }[];
-            keys: { id: string }[];
-            requiresHdKey: boolean;
-          }>(
-            (all, entries) => {
-              const [id, variable] = entries;
-              if (variable.type === 'WalletData') {
-                return { ...all, walletData: [...all.walletData, { id }] };
-              }
-              if (variable.type === 'AddressData') {
-                return { ...all, addressData: [...all.addressData, { id }] };
-              }
-              if (variable.type === 'Key') {
-                return { ...all, keys: [...all.keys, { id }] };
-              }
-              // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-              if (variable.type !== 'HdKey') {
-                log.fatal(
-                  `The provided template requires an unknown variable type: "${
-                    (variable as { type: string }).type
-                  }".`
-                );
-              }
-              return { ...all, requiresHdKey: true };
-            },
-            {
-              addressData: [],
-              keys: [],
-              requiresHdKey: false,
-              walletData: [],
-            }
+    const partitionedVariables = Object.entries(entity.variables).reduce<{
+      addressData: string[];
+      walletData: string[];
+      keys: string[];
+      requiresHdKey: boolean;
+      hdPublicKeyDerivationPath: string;
+    }>(
+      // eslint-disable-next-line complexity
+      (all, entries) => {
+        const [id, variable] = entries;
+        if (variable.type === 'WalletData') {
+          return { ...all, walletData: [...all.walletData, id] };
+        }
+        if (variable.type === 'AddressData') {
+          return { ...all, addressData: [...all.addressData, id] };
+        }
+        if (variable.type === 'Key') {
+          return { ...all, keys: [...all.keys, id] };
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+        if (variable.type !== 'HdKey') {
+          log.fatal(
+            `The provided template requires an unknown variable type: "${
+              (variable as { type: string }).type
+            }".`
           );
-
-    if (partitionedVariables === undefined) {
-      // TODO:
-      return undefined;
-    }
+        }
+        return {
+          ...all,
+          hdPublicKeyDerivationPath:
+            variable.hdPublicKeyDerivationPath ?? all.hdPublicKeyDerivationPath,
+          requiresHdKey: true,
+        };
+      },
+      {
+        addressData: [],
+        hdPublicKeyDerivationPath: CompilerDefaults.hdKeyHdPublicKeyDerivationPath as string,
+        keys: [],
+        requiresHdKey: false,
+        walletData: [],
+      }
+    );
 
     const hasAddressData = partitionedVariables.addressData.length > 0;
     const hasWalletData = partitionedVariables.walletData.length > 0;
@@ -213,18 +309,147 @@ export default class WalletNew extends Command {
       // eslint-disable-next-line no-console
       console.log(
         colors.bold.red(
-          'WARNING: this wallet template requires custom variables – Bitauth CLI does not yet support "dry-run" testing, so invalid variables may prevent funds from being spendable. Test this wallet carefully before using it on mainnet.'
+          'WARNING: this wallet template requires custom variables – Bitauth CLI does not yet support "dry-run" testing, so invalid variables may permanently prevent funds from being spendable. Test this wallet carefully before using it on mainnet.'
         )
       );
     }
 
-    // check that all address data and wallet data is provided (and no unexpected properties were provided)
+    const walletDataResult = hasWalletData
+      ? verifyDataItem(walletData, partitionedVariables.walletData)
+      : undefined;
+
+    if (Array.isArray(walletDataResult)) {
+      return log.fatal(
+        `The provided wallet data is invalid: ${walletDataResult.join(' ')}`
+      );
+    }
+
+    const addressDataResult = hasAddressData
+      ? verifyAddressData(addressData, partitionedVariables.addressData)
+      : undefined;
+
+    if (addressDataResult !== undefined && !addressDataResult.success) {
+      return log.fatal(
+        `The provided address data is invalid: ${addressDataResult.errors.join(
+          ' '
+        )}`
+      );
+    }
+
+    const [ripemd160, secp256k1, sha256, sha512] = await Promise.all([
+      ripemd160Promise,
+      secp256k1Promise,
+      sha256Promise,
+      sha512Promise,
+    ]);
+
+    const getKeyPair = (id: string) => {
+      const privateKey = generatePrivateKey(random32Bytes);
+      const publicKey = secp256k1.derivePublicKeyCompressed(privateKey);
+      return { id, privateKey, publicKey };
+    };
+    const keys = partitionedVariables.keys.map(getKeyPair);
+
+    const hasMultipleEntities = Object.keys(template.entities).length > 1;
+    const entityMessagingKey = hasMultipleEntities
+      ? getKeyPair('entityMessagingKey')
+      : undefined;
+
+    const privateKeys = keys.reduce(
+      (all, pair) => ({ ...all, [pair.id]: pair.privateKey }),
+      {}
+    );
+    const publicKeys = keys.reduce(
+      (all, pair) => ({ ...all, [pair.id]: pair.publicKey }),
+      {}
+    );
+
+    const hdKey = partitionedVariables.requiresHdKey
+      ? (() => {
+          const { node, seed } = generateHdPrivateNode(
+            { sha512 },
+            random32Bytes
+          );
+          const privateKey = encodeHdPrivateKey(
+            { sha256 },
+            { network: 'mainnet', node }
+          );
+          const publicKeyDerivationPathPrivateNode = deriveHdPath(
+            { ripemd160, secp256k1, sha256, sha512 },
+            node,
+            partitionedVariables.hdPublicKeyDerivationPath
+          );
+          if (typeof publicKeyDerivationPathPrivateNode === 'string') {
+            return log.fatal(
+              `There was a problem deriving the HD public key: ${publicKeyDerivationPathPrivateNode}`
+            );
+          }
+          const publicKey = encodeHdPublicKey(
+            { sha256 },
+            {
+              network: 'mainnet',
+              node: deriveHdPublicNode(
+                { secp256k1 },
+                publicKeyDerivationPathPrivateNode
+              ),
+            }
+          );
+          return {
+            privateKey,
+            publicKey,
+            seed,
+          };
+        })()
+      : undefined;
+
+    const walletShare = {
+      [entityId]: {
+        addressData: addressDataResult?.addressData,
+        createdAt: new Date().toISOString(),
+        hdPublicKey: hdKey?.publicKey,
+        publicKeys,
+        template,
+        templateAlias,
+        walletAlias,
+        walletData: walletDataResult,
+        walletName,
+      },
+    };
+
+    const walletShareSigningSerialization = formatJsonForSigning(walletShare);
+
+    const proposal = {
+      messagingKeys: entityMessagingKey
+        ? {
+            [entityId]: entityMessagingKey.publicKey,
+          }
+        : undefined,
+      shareSignatures: {},
+      walletShares: {
+        [entityId]: walletShare,
+      },
+    };
+
+    const walletSecret = {
+      privateData: {
+        hdKey:
+          hdKey === undefined
+            ? undefined
+            : {
+                private: hdKey.privateKey,
+                seed: hdKey.seed,
+              },
+        messagingKey: entityMessagingKey?.privateKey,
+        privateKeys,
+      },
+      proposal,
+    };
 
     // fill key values
 
     // output `wallet-secret.json` - keep keys in separate property from wallet data (to make more interchangeable once wallet groups land)
 
-    // output `${workingDirectory}/${alias}-wallet-invitation.json` with everything but the keys.
+    // output `${workingDirectory}/${alias}-wallet-proposal.json` with everything but the keys.
 
     this.log('TODO: run command');
     return undefined;
--- src/internal/formatting.ts
+++ src/internal/formatting.ts
@@ -1,13 +1,42 @@
+import { sortObjectKeys, utf8ToBin } from '@bitauth/libauth';
 import * as colors from 'ansi-colors';
 
 export { colors };
 
 const jsonPadding = 2;
-export const formatJson = (json: unknown) =>
+/**
+ * Format a JSON value for output.
+ * @param json - the value to serialize
+ */
+export const formatJsonUnsorted = (json: unknown) =>
   JSON.stringify(json, null, jsonPadding);
 
+/**
+ * Canonically sort and format a JSON value for output.
+ * @param json - the value to serialize
+ */
+export const formatJson = (json: unknown) =>
+  formatJsonUnsorted(sortObjectKeys(json, true));
+
+/**
+ * Canonically sort and serialize a JSON value to produce a preimage for
+ * signing.
+ * @param json - the value to serialize
+ */
+export const serializeJsonForSigning = (json: unknown) =>
+  utf8ToBin(JSON.stringify(sortObjectKeys(json, true), null, 0));
+
+/**
+ * Covert any string to `kebab-case`,the recommended format for all types of
+ * aliases.
+ * @param text - the string to convert
+ */
 export const toKebabCase = (text: string) =>
   text.toLowerCase().replace(/[^a-z0-9]/gu, '-');
 
+/**
+ * Escape a string for use in a bash command.
+ * @param bashString - the string to escape
+ */
 export const bashEscapeSingleQuote = (bashString: string) =>
   bashString.replace(/\\/gu, '\\').replace(/'/gu, "'\\''");
